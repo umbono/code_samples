d9988b10d6cd95a137d6be87d52dd9dea673f0ae
Ravi <rtiwari@paylynxs.com>
2023-01-23 17:28:12 -0700

    updated ws and api code

--- api_vs_socket/samplews.py
+++ api_vs_socket/samplews.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket
+from starlette.websockets import WebSocketDisconnect
 import uvicorn
 
 app = FastAPI()
@@ -7,8 +8,12 @@ app = FastAPI()
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(f"Message text was: {data}")
+        try:
+            data = await websocket.receive_text()
+            await websocket.send_text(f"Message text was: {data}")
+        except WebSocketDisconnect:
+            print("WebSocket connection closed.")
+            break
         
 if __name__ == '__main__':
     uvicorn.run("samplews:app", host="0.0.0.0", port=5001, reload=True, workers=2)
